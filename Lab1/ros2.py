8e32784aa7ec7375c9af2d626811fd1a0ceb8522
Mingyang Yu <mingyangyu@Mingyangs-MacBook-Pro-5.local>
2026-01-30 13:52:02 -0500

    i

--- Lab1/ros2.py
+++ Lab1/ros2.py
@@ -9,36 +9,20 @@ class MyNode(hm.HelloNode):
     def main(self):
         hm.HelloNode.main(self, 'my_node', 'my_node', wait_for_first_pointcloud=False)
 
-        self.move_to_pose({
-    'lift': 0.3,
-    'arm': 0.0,
-    'wrist_yaw': 0.0,
-    'wrist_pitch': 0.0,
-    'wrist_roll': 0.0,
-    'joint_head_pan': 0.0,
-    'joint_head_tilt': -0.8,
-    'gripper_aperture': 0.0
-                                }, blocking=True)   
+        self.stow_the_robot()   
         self.move_to_pose({'joint_arm': 0.52},{'joint_lift': 1.1})
         self.move_to_pose({'joint_wrist_yaw': -1.0}, blocking=True)
         self.move_to_pose({'joint_wrist_pitch': -1.0}, blocking=True)
         self.move_to_pose({'joint_wrist_roll': -1.0}, blocking=True)
-    #     self.move_to_pose({'gripper_aperture': 0.05}, blocking=True)
-    #     self.move_to_pose({'gripper_aperture': 0.0}, blocking=True)
-    #     self.move_to_pose({'joint_head_pan': 0.0, 'joint_head_tilt': -0.3}, blocking=True)
-    #     self.move_to_pose({
-    # 'lift': 0.3,
-    # 'arm': 0.0,
-    # 'wrist_yaw': 0.0,
-    # 'wrist_pitch': 0.0,
-    # 'wrist_roll': 0.0,
-    # 'joint_head_pan': 0.0,
-    # 'joint_head_tilt': -0.8,
-    # 'gripper_aperture': 0.0
-    #                             }, blocking=True)
-    #     self.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
-    #     self.move_to_pose({'rotate_mobile_base': np.deg2rad(180)}, blocking=True)
-    #     self.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+        self.move_to_pose({'gripper_aperture': 0.05}, blocking=True)
+        self.move_to_pose({'gripper_aperture': 0.0}, blocking=True)
+        self.move_to_pose({'joint_head_pan': 0.0, 'joint_head_tilt': -0.3}, blocking=True)
+        self.stow_the_robot()
+        self.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+        self.move_to_pose({'rotate_mobile_base': np.deg2rad(180)}, blocking=True)
+        self.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+        self.destory_node()
 
-node = MyNode()
-node.main()
+if __name__ == 'main':
+    node = MyNode()
+    node.main()
